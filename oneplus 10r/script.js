6d3517af1008640eeddd4d57a403e36241150cbd
eshvayko <shvayko.zhenya@gmail.com>
2024-09-17 21:04:51 +0300

    обова ванплас

---
+++ oneplus 10r/script.js
@@ -0,0 +1,46 @@
+var days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+var months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+const dayweek = document.querySelector('.dweek');
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
+
+var weeklast, hourslast, minuteslast, datelast, monthlast;
+
+function checkZero(time) {
+    if (time < 10) {
+        return `0${time}`;
+    } else {
+        return time;
+    }
+}
+
+function checkRedOne(time) {
+    if (time > 9 && time < 20) {
+        if (time == 11) {
+            return '<span class="one">11</span>';
+        } else {
+            return `<span class="one">1</span>${time - 10}`;
+        }
+    } else if (time == 21) {
+        return `2<span class="one">1</span>`;
+    } else {
+        return time;
+    }
+}
+
+function update() {
+    var data = new Date();
+    if (weeklast != data.getDay() || hourslast != data.getHours() || minuteslast != data.getMinutes() || datelast != data.getDate() || monthlast != data.getMonth()) {
+        dayweek.innerHTML = days[data.getDay()];
+        time.innerHTML = `${checkZero(checkRedOne(data.getHours()))}:${checkZero(data.getMinutes())}`;
+        date.innerHTML = `<span class="twone">${data.getDate()}</span> ${months[data.getMonth()]}`;
+        weeklast = data.getDay();
+        hourslast = data.getHours();
+        minuteslast = data.getMinutes();
+        datelast = data.getDate();
+        monthlast = data.getMonth();
+    }
+}
+
+setInterval(update, 100);
