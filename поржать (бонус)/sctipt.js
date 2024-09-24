a82d58c6d63be59b3b4e2acac5b6f7e16eb027d6
eshvayko <shvayko.zhenya@gmail.com>
2024-09-24 20:15:05 +0300

    просто

--- поржать (бонус)/sctipt.js
+++ поржать (бонус)/sctipt.js
@@ -1,11 +1,11 @@
 document.querySelector('input').addEventListener('change', () => {
-    var buttonInf = document.querySelector('#buttoninf');
-    var name = document.querySelector("#inputfile").files[0].name;
-    if (name.substring(name.length - 4, name.length) == '.wav' || name.substring(name.length - 4, name.length) == '.mp3') {
+    const buttonInf = document.querySelector('#buttoninf');
+    const name = document.querySelector("#inputfile").files[0].name;
+    if (name.substring(name.length - 4, name.length) === '.wav' || name.substring(name.length - 4, name.length) === '.mp3') {
         buttonInf.innerHTML = '<button id="button">поржать</button>';
-        var button = document.querySelector('#button');
+        const button = document.querySelector('#button');
         document.querySelector('#autoplaysettings').innerHTML = '<button id="autosmehBtn">натроить автосмех</button>';
-        var buttonautosmeh = document.querySelector('#autosmehBtn');
+        const buttonautosmeh = document.querySelector('#autosmehBtn');
         buttonautosmeh.addEventListener('click', autoplayAudio);
         button.addEventListener('click', playAudio);
     } else {
@@ -15,30 +15,30 @@ document.querySelector('input').addEventListener('change', () => {
 
 function playAudio() {
     document.querySelector('#autoplaysettings').innerHTML = '';
-    var audio = new Audio(`ауди/${document.querySelector("#inputfile").files[0].name}`);
+    const audio = new Audio(`ауди/${document.querySelector("#inputfile").files[0].name}`);
     audio.play();
 }
 
 function autoplayAudio() {
     document.querySelector('#buttoninf').innerHTML = '';
-    document.querySelector('#autoplaysettings').innerHTML = 'Скороть запуска аудио: <input type="number" id="inputspeed" placeholder="2раз/с"><br><br>Время прололжения запуска аудио: <input type="number" id="inputtime" placeholder="5c"></input><br><br><button id="button">автопоржать</button>';
+    document.querySelector('#autoplaysettings').innerHTML = 'Скороть запуска аудио: <input type="number" id="inputspeed" placeholder="2раз/с"><br><br>Время прололжения запуска аудио: <input type="number" id="inputtime" placeholder="5c"><br><br><button id="button">автопоржать</button>';
     document.querySelector('#button').addEventListener('click', () => {
-        var timeInput = document.querySelector('#inputtime');
-        var speedInput = document.querySelector('#inputspeed');
-        if (timeInput.value == '' || timeInput.value <= 0) {
+        const timeInput = document.querySelector('#inputtime');
+        const speedInput = document.querySelector('#inputspeed');
+        if (timeInput.value === '' || timeInput.value <= 0) {
             window.time = 5;
         } else {
             window.time = timeInput.value;
         }
-        if (speedInput.value == '' || speedInput.value <= 0) {
+        if (speedInput.value === '' || speedInput.value <= 0) {
             window.speed = 2;
         } else {
             window.speed = speedInput.value;
         }
         timeInput.value = '';
         speedInput.value = '';
-        var autoplay = setInterval(() => {
-            var audio = new Audio(`ауди/${document.querySelector("#inputfile").files[0].name}`);
+        const autoplay = setInterval(() => {
+            const audio = new Audio(`ауди/${document.querySelector("#inputfile").files[0].name}`);
             audio.play();
         }, 1000 / speed)
         setTimeout(() => {clearInterval(autoplay);}, time * 1000);
