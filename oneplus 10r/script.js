3c19aff177da404bbda37d6884685a3f02a1fd2f
eshvayko <shvayko.zhenya@gmail.com>
2024-11-02 17:35:08 +0300

    global update

--- oneplus 10r/script.js
+++ oneplus 10r/script.js
@@ -1,11 +1,103 @@
-var days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
-var months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+let days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
-const dayweek = document.querySelector('.dweek');
-const time = document.querySelector('.time');
-const date = document.querySelector('.date');
+let contentNow = 1;
 
-var weeklast, hourslast, minuteslast, datelast, monthlast;
+let weeklast, hourslast, minuteslast, datelast, monthlast, contentNowLast;
+
+const updateTimeout = setInterval(update, 50);
+
+let batteryLvl = Math.floor(Math.random() * 101) + 2;
+showBatteryLvl();
+if (batteryLvl > 0) {
+    window.timeout = setInterval(showBatteryLvl, 10000);
+}
+function showBatteryLvl() {
+    batteryLvl--;
+    if (batteryLvl > 100) {
+        batteryLvl = 100;
+    }
+    let batteryIcon;
+    if (batteryLvl <= 0) {
+        document.querySelector(`.content-${contentNow}`).innerHTML = 'а все 0%';
+        document.querySelector('.oneplus').style.background = '';
+        document.querySelector('.finger').remove();
+        clearInterval(timeout);
+        clearInterval(updateTimeout);
+        return;
+    }else if (batteryLvl < 10) {
+        batteryIcon = 0;
+    } else if (batteryLvl < 25) {
+        batteryIcon = 1;
+    } else if (batteryLvl < 60) {
+        batteryIcon = 2;
+    } else if (batteryLvl < 90) {
+        batteryIcon = 3;
+    } else if (batteryLvl >= 90) {
+        batteryIcon = 4;
+    }
+    document.querySelector(`.content-${contentNow} .batteryInfo`).innerHTML = `<i class="i-battery-${batteryIcon} i-battery"></i> ${batteryLvl} %`;
+}
+
+let i = localStorage.getItem('i');
+let timeGo = false;
+showAnimation(1);
+function showAnimation(time) {
+    if (!(timeGo && contentNow !== 1)) {
+        let positions = [47, 142, 210, 275, 345];
+        function show() {
+            timeGo = false;
+            if (contentNow === 1) {
+                document.querySelector('.content-1').style.marginTop = `${positions[i]}px`;
+                document.querySelector('.content-1').style.animation = 'appear 0.9s forwards';
+                document.querySelector('.i-finger').style.animation = 'appear-finger 0.9s forwards';
+                i++;
+                if (i >= positions.length) {
+                    i = 0;
+                }
+                localStorage.setItem('i', i);
+            }
+        }
+        setTimeout(show, time * 1000);
+        timeGo = true;
+    }
+}
+
+function showWeather() {
+    let weathers = ['precipitation', 'clear', 'cloud', 'thunderstorm'];
+    let temperature = Math.floor(Math.random() * 31) * [1, -1][Math.floor(Math.random() * 2)];
+    if (temperature === -0) {
+        temperature = 0;
+    }
+    if (temperature < 0) {
+        weathers.pop();
+    }
+    let tempDay = temperature + Math.floor(Math.random() * 5);
+    let tempNight = temperature - Math.floor(Math.random() * 5);
+    let weather = weathers[Math.floor(Math.random() * weathers.length)];
+    if (weather === 'precipitation' && temperature < 0) {
+        weather = 'snow';
+    } else if (temperature >= 0) {
+        weather = 'rain';
+    } else if (weather === 'clear' && new Date().getHours() >= 21) {
+        weather = 'moon';
+    } else if (weather === 'clear' && new Date().getHours() < 21) {
+        weather = 'sun';
+    }
+    if (temperature > 0) {
+        temperature = `+${temperature}`;
+    }
+    if (tempDay > 0) {
+        tempDay = `+${tempDay}`;
+    }
+    if (tempNight > 0) {
+        tempNight = `+${tempNight}`;
+    }
+    document.querySelector('#weather').innerHTML = `<i class="i-${weather} weather"></i>`;
+    document.querySelector('#tempNow').innerHTML = `${temperature}&deg;`;
+    document.querySelector('#tempDay').innerHTML = `${tempDay}&deg;`;
+    document.querySelector('#tempNight').innerHTML = `${tempNight}&deg;`;
+}
 
 function checkZero(time) {
     if (time < 10) {
@@ -16,37 +108,101 @@ function checkZero(time) {
 }
 
 function checkRedOne(time) {
+    let className;
+    if (contentNow > 1) {
+        className = 'one-1';
+    } else {
+        className = 'one';
+    }
     if (time > 9 && time < 20) {
-        if (time == 11) {
-            return '<span class="one">11</span>';
+        if (time === 11) {
+            return `<span class="${className}">11</span>`;
         } else {
-            return `<span class="one">1</span>${time - 10}`;
+            return `<span class="${className}">1</span>${time - 10}`;
         }
-    } else if (time == 21) {
-        return `2<span class="one">1</span>`;
+    } else if (time === 21) {
+        return `2<span class="${className}">1</span>`;
     } else {
         return time;
     }
 }
 
 function update() {
-    var data = new Date();
-    if (weeklast != data.getDay() || hourslast != data.getHours() || minuteslast != data.getMinutes() || datelast != data.getDate() || monthlast != data.getMonth()) {
+    if (batteryLvl <= 0) {
+        return;
+    }
+    let className;
+    if (contentNow > 1) {
+        className = 'twone-1';
+    } else {
+        className = 'twone';
+    }
+    const dayweek = document.querySelector(`.content-${contentNow} .dweek`);
+    const time = document.querySelector(`.content-${contentNow} .time`);
+    const date = document.querySelector(`.content-${contentNow} .date`);
+    let data = new Date();
+    if (weeklast !== data.getDay() || hourslast !== data.getHours() || minuteslast !== data.getMinutes() || datelast !== data.getDate() || monthlast !== data.getMonth() || contentNowLast !== contentNow) {
         dayweek.innerHTML = days[data.getDay()];
         time.innerHTML = `${checkZero(checkRedOne(data.getHours()))}:${checkZero(data.getMinutes())}`;
-        date.innerHTML = `<span class="twone">${data.getDate()}</span> ${months[data.getMonth()]}`;
+        if (contentNow === 3) {
+            document.querySelector('#time').innerHTML = `${checkZero(data.getHours())}:${checkZero(data.getMinutes())}`;
+        }
+        date.innerHTML = `<span class="${className}">${data.getDate()}</span> ${months[data.getMonth()]}`;
         weeklast = data.getDay();
         hourslast = data.getHours();
         minuteslast = data.getMinutes();
         datelast = data.getDate();
         monthlast = data.getMonth();
+        contentNowLast = contentNow;
+        batteryLvl++;
+        showBatteryLvl();
     }
 }
 
-setInterval(update, 100);
-
 function unlock() {
-    document.querySelector('.oneplus').innerHTML = '<div class="right_btn"></div><div class="left_btn_one"></div><div class="left_btn_two"></div><div class="circle"><div class="center-circle"></div></div>';
-    // document.querySelector('.oneplus').style = 'background-image: url(images/максим2.jpg);';
-    document.querySelector('.oneplus').style = 'background-image: url(images/кириешки.png);';
+    if (batteryLvl <= 0) {
+        return;
+    }
+    if (contentNow === 1 || contentNow === 2) {
+        document.querySelector('.oneplus').style = 'background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(images/wall-2.jpg) no-repeat center; image-rendering: pixelated; background-size: 350px;'; // image-rendering: pixelated - убирает размытие
+        document.querySelector('.content-1').style = 'display: none';
+        document.querySelector('.content-2').style = 'display: none';
+        document.querySelector('.content-3').style = 'display: block';
+        document.querySelector('.finger').style = 'display: none';
+        contentNow = 3;
+        showWeather();
+    }
+
+}
+
+function turnOn() {
+    if (batteryLvl <= 0) {
+        return;
+    }
+    if (contentNow === 3) {
+        document.querySelector('.oneplus').style = 'background: ';
+        document.querySelector('.content-3').style = 'display: none;';
+        document.querySelector('.content-2').style = 'display: none;';
+        document.querySelector('.content-1').style = 'display: block;';
+        document.querySelector('.finger').style = 'display: block';
+        document.querySelector('.i-finger').style = 'opacity: 0';
+        showAnimation(1.8);
+        contentNow = 1;
+
+    } else if (contentNow === 2) {
+        document.querySelector('.oneplus').style = 'background: ';
+        document.querySelector('.content-3').style = 'display: none;';
+        document.querySelector('.content-2').style = 'display: none;';
+        document.querySelector('.content-1').style = 'display: block;';
+        document.querySelector('.i-finger').style = 'opacity: 0';
+        showAnimation(1.8);
+        contentNow = 1;
+    } else {
+        document.querySelector('.oneplus').style = 'background: url(images/wall.jpg) no-repeat center; background-size: 455px;';
+        document.querySelector('.i-finger').style = 'opacity: 1';
+        document.querySelector('.content-3').style = 'display: none;';
+        document.querySelector('.content-1').style = 'display: none;';
+        document.querySelector('.content-2').style = 'display: block;';
+        contentNow = 2;
+    }
 }
--- todo list/script.js
+++ todo list/script.js
@@ -2,14 +2,38 @@ const input = document.querySelector('input');
 const persentInfo = document.querySelector('.persent');
 const list = document.querySelector('.list');
 
-let tasks = 0;
-let maxtasks = tasks;
-let completeTasks = 0;
-countPersent()
+if (localStorage.getItem('maxtasks') === undefined) {
+    localStorage.setItem('maxtasks', 0);
+}
+let tasks = localStorage.length - 1;
+let maxtasks = localStorage.getItem('maxtasks');
+loadTasks();
+let completeTasks = countCompleteTasks();
+countPersent();
 
 document.onkeydown = (event) => {
     if (event.keyCode === 13) {
         addTask();
+    }
+}
+
+function countCompleteTasks() {
+    let ct = 0;
+    for (let num = 0; num <= maxtasks; num++) {
+        try {
+            if (document.querySelector(`.task${num} .text`).classList.contains('checked')) {
+                ct++;
+            }
+        } catch {}
+    }
+    return ct;
+}
+
+function loadTasks() {
+    for (let num = 0; num <= maxtasks; num++) {
+        if (localStorage.getItem(`task${num}`) !== null) {
+            list.innerHTML += localStorage.getItem(`task${num}`);
+        }
     }
 }
 
@@ -24,10 +48,12 @@ function countPersent() {
 function addTask() {
     if (input.value !== '') {
         list.innerHTML += `<li class="task${tasks}"><i class="i-check" onclick="checkTask(${tasks})" style="margin-right:10px"></i><span class="text">${input.value}</span><span class="buttonsli"><i class="i-pen" onclick="editTask(${tasks})" style="margin-right: 10px"></i><i class="i-trash-can" onclick="deleteTask(${tasks})"></i></span></li>`;
+        localStorage.setItem(`task${tasks}`, `<li class="task${tasks}"><i class="i-check" onclick="checkTask(${tasks})" style="margin-right:10px"></i><span class="text">${input.value}</span><span class="buttonsli"><i class="i-pen" onclick="editTask(${tasks})" style="margin-right: 10px"></i><i class="i-trash-can" onclick="deleteTask(${tasks})"></i></span></li>`);
         input.value = '';
         tasks++;
         if (tasks > maxtasks) {
             maxtasks = tasks;
+            localStorage.setItem('maxtasks', maxtasks);
         }
         countPersent();
     }
@@ -40,6 +66,7 @@ function deleteTask(num) {
     document.querySelector(`.task${num}`).remove();
     tasks--;
     countPersent();
+    localStorage.removeItem(`task${num}`);
 }
 
 function deleteAll() {
@@ -47,6 +74,7 @@ function deleteAll() {
     tasks = 0;
     completeTasks = 0;
     countPersent();
+    localStorage.clear();
 }
 
 function checkTask(num) {
@@ -55,17 +83,19 @@ function checkTask(num) {
         document.querySelector(`.task${num} .i-check`).className += ' checked-2';
         completeTasks++;
         countPersent();
+        localStorage.setItem(`task${num}`, `<li class="task${num}"><i class="i-check checked-2" onclick="checkTask(${num})" style="margin-right:10px"></i><span class="text checked">${document.querySelector(`.task${num} .text`).innerHTML}</span><span class="buttonsli"><i class="i-pen" onclick="editTask(${num})" style="margin-right: 10px"></i><i class="i-trash-can" onclick="deleteTask(${num})"></i></span></li>`);
     }
 }
 
 function deleteDoneTasks() {
-    for (let  num = 0; num <= maxtasks; num++) {
+    for (let num = 0; num <= maxtasks; num++) {
         try {
             if (document.querySelector(`.task${num} .text`).classList.contains('checked')) {
                 document.querySelector(`.task${num}`).remove();
                 completeTasks--;
                 tasks--;
                 countPersent();
+                localStorage.removeItem(`task${num}`);
             }
         } catch {}
     }
@@ -74,6 +104,11 @@ function deleteDoneTasks() {
 function editTask(num) {
     if (input.value !== '') {
         document.querySelector(`.task${num} .text`).innerHTML = input.value;
+        if (!document.querySelector(`.task${num} .text`).classList.contains('checked')) {
+            localStorage.setItem(`task${num}`, `<li class="task${num}"><i class="i-check" onclick="checkTask(${num})" style="margin-right:10px"></i><span class="text">${input.value}</span><span class="buttonsli"><i class="i-pen" onclick="editTask(${num})" style="margin-right: 10px"></i><i class="i-trash-can" onclick="deleteTask(${num})"></i></span></li>`);
+        } else {
+            localStorage.setItem(`task${num}`, `<li class="task${num}"><i class="i-check checked-2" onclick="checkTask(${num})" style="margin-right:10px"></i><span class="text checked">${input.value}</span><span class="buttonsli"><i class="i-pen" onclick="editTask(${num})" style="margin-right: 10px"></i><i class="i-trash-can" onclick="deleteTask(${num})"></i></span></li>`);
+        }
         input.value = '';
     }
 }
